05854aa42a153f875a3776ef051226bb70e00240
Regina Alyssa <reginaalyssa01809@gmail.com>
2021-04-04 01:51:11 +0800

    Refactor surfacing of share button on mobile active share popup

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -1,21 +1,31 @@
 console.log("yay");
 
-let shareButton = document.getElementsByClassName("btn--share")[0];
+let shareButtons = document.getElementsByClassName("btn--share");
 
-shareButton.onclick = function toggleShareContainer(e) {
+function toggleShareContainer(e) {
+	console.log("YO");
 	shareButtonElement = e.target.closest(".btn--share");
 	shareButtonElement.classList.toggle("btn--share_active");
 	
 	let sharePopup = shareButtonElement.nextElementSibling;
+	console.log(sharePopup);
+	if (sharePopup == null) { sharePopup = shareButtonElement.closest(".article-card__share-popup"); }
 	sharePopup.classList.toggle("article-card__share-popup_visible");
+
+	let footer = shareButtonElement.closest(".article-card__footer");
+	footer.classList.toggle("article-card__footer_share-active");
 }
 
-document.addEventListener('keyup', hideButtonOutline);
+for (var i = 0; i < shareButtons.length; i++) {
+	shareButtons[i].addEventListener("click", toggleShareContainer);
+}
+
+document.addEventListener("keyup", hideButtonOutline);
 
 function hideButtonOutline(e) {
   if (e.keyCode == 9) {
 	let shareButtonElement = document.getElementsByClassName("btn--share")[0];
 	shareButtonElement.classList.add("btn--share_with-outline");
-	document.removeEventListener('keyup', hideButtonOutline);
+	document.removeEventListener("keyup", hideButtonOutline);
   }
 }
